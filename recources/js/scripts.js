cd156826049eed56a7ae76a1a51e54d82e5dea3f
Patrycja Kubińska <kubinskapatrycja@gmail.com>
2021-04-27 13:20:18 +0200

    Add scroll to element.

---
+++ recources/js/scripts.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+  const HEADER_HEIGHT = 59;
+
+  // For the sticky navigation
+  $(".js--section-features").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    {
+      offset: "60px;",
+    }
+  );
+
+  // Scroll on buttons
+  $(".js--scroll-to-plans").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-plans").offset().top - HEADER_HEIGHT },
+      2500
+    );
+  });
+
+  $(".js-scroll-to-start").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-features").offset().top - HEADER_HEIGHT },
+      1000
+    );
+  });
+
+  // Navigation scroll
+
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function () {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top - HEADER_HEIGHT,
+            },
+            1000
+          );
+          return false;
+        }
+      }
+    });
+});
